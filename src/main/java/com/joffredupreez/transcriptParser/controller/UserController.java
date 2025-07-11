07985fe3fc780379751d9dd35ab8fbf76afbd946
JoffduPreez <joffre@joffredupreez.com>
2025-07-11 12:14:18 -0400

    added update profile endpoint

--- src/main/java/com/joffredupreez/transcriptParser/controller/UserController.java
+++ src/main/java/com/joffredupreez/transcriptParser/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.joffredupreez.transcriptParser.controller;
 
 import com.joffredupreez.transcriptParser.model.AppUser;
+import com.joffredupreez.transcriptParser.repositiory.AppUserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +17,19 @@ public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
+    @Autowired
+    private AppUserRepository userRepo;
+
     @GetMapping("/profile")
     public ResponseEntity<?> getUserProfile(@AuthenticationPrincipal AppUser user) {
+        return ResponseEntity.ok(new ProfileResponse(user.getUsername(), user.getEmail()));
+    }
+
+    @PutMapping("/profile")
+    public ResponseEntity<?> updateUserProfile(@AuthenticationPrincipal AppUser user, @RequestBody UpdateProfileRequest updateRequest) {
+        user.setUsername(updateRequest.username);
+        user.setEmail(updateRequest.email);
+        userRepo.save(user);
         return ResponseEntity.ok(new ProfileResponse(user.getUsername(), user.getEmail()));
     }
 
@@ -26,5 +41,10 @@ public class UserController {
             this.username = username;
             this.email = email;
         }
+    }
+
+    public static class UpdateProfileRequest {
+        public String username;
+        public String email;
     }
 }
